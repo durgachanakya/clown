9a2c9542d627feb6258a73b7382fab4b60dbecbf
durgachanakya <81852237+durgachanakya@users.noreply.github.com>
2022-03-28 20:39:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
-function preload(){
+noseX = 0;
+noseY = 0;
 
+
+function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png')
 }
 
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300) ;
+    video.hide();
+
+    posenet = ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotposes);
+}
+
+function modelLoaded(){
+    console.log("posenet is loaded");
+}
+
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-15;
+        console.log("nosex = " + noseX + "nosey =" + noseY);
+    }
 }
 
 function draw(){
-
+    image(video,0,0,300,300);
+    image(clown_nose,noseX,noseY,30,30);
 }
 
 function take_snapshot(){
